37fec0d3916817ccdfe1ad8cb77200b20f8bbd24
CupOfTea696 <cupoftea696@gmail.com>
2016-02-02 20:26:25 +0000

    Generate decks meta file

--- app/Console/Commands/GenerateDecksMetaFile.php
+++ app/Console/Commands/GenerateDecksMetaFile.php
@@ -48,7 +48,9 @@ class GenerateDecksMetaFile extends Command
             $meta = [];
         }
         
-        $decks = collect(Storage::files('decks'))->map(function ($file) {
+        $decks = Storage::files('decks');
+        
+        $decks = collect($decks)->map(function ($file, $key) {
             $deck = json_decode(Storage::get($file), true);
             $deck['file'] = str_replace('decks/', '', $file);
             
@@ -57,49 +59,56 @@ class GenerateDecksMetaFile extends Command
             return $deck;
         });
         
-        $meta['main'] = new DeckMeta($decks->pull($decks->search(function ($deck) {
+        $main = $decks->pull($decks->search(function ($deck) {
             return $deck['name'] == 'Cards Against Humanity';
-        })));
+        }));
         
-        $meta['main']->add([
+        $main = DeckMeta::make($main);
+        
+        $meta['main'] = $main->add([
             'id' => 'main',
-            'locales' => $this->determineLocalesPosition($main['locales'], array_get($meta, 'main.locales', [])),
+            'locales' => $this->determineLocalesPosition($main->locales, array_get($meta, 'main.locales', [])),
             'source' => 'file',
             'type' => 'official',
-        ]);
+        ])->toArray();
         
         $decks = $decks->map(function ($deck) use ($meta) {
             $deck_meta = $this->getMeta($deck, $meta);
             
             if ($deck_meta) {
-                $deck_id = array_first(array_keys($deck_meta));
-                $deck_meta = array_first($deck_meta);
+                $deck_id = head(array_keys($deck_meta));
+                $deck_meta = head($deck_meta);
                 
                 $deck['id'] = $deck_id;
                 $deck['type'] = $deck_meta['type'];
                 $deck['meta'] = $deck_meta;
             } else {
-                $deck['id'] = $this->ask('Enter an ID for the deck <comment>' . $deck['name'] . '</comment>', strtolower($deck['slug']));
-                $deck['type'] = $this->choice('Specify deck type.', ['o' => 'official', 'p' => 'pax', 3 => 'thirdParty'], 'o');
+                $deck['id'] = $this->ask('Enter an ID for the deck <comment>' . $deck['name'] . '</comment>', str_slug(strtolower($deck['slug']), '_'));
+                $deck['type'] = $this->anticipate('Specify deck type.', ['official' => 'o', 'pax' => 'p', 'thirdParty' => 3], 'official');
                 $deck['meta'] = false;
             }
             
             return $deck;
         });
         
-        // official
-        $official = $decks->where('type', 'official')->values();
-        $official = $this->sortSet($official, array_get($meta, 'official', []));
+        // file based
         
-        // pax
-        $pax = $decks->where('type', 'pax')->values();
-        $pax = $this->sortSet($pax, array_get($meta, 'pax', []));
+        foreach ($decks->pluck('type')->unique() as $type) {
+            $set = $decks->where('type', $type)->values();
+            $set = $this->sortSet($set, array_get($meta, $type, []));
+            $set = $set->keyBy('id');
+            $set = $set->map(function ($deck) {
+                return DeckMeta::make($deck)->toArray();
+            });
+            
+            $meta[$type] = $set;
+        }
         
-        // thirdParty
-        $third = $decks->where('type', 'thirdParty')->values();
-        $third = $this->sortSet($third, array_get($meta, 'thirdParty', []));
+        // currated / cardcast
         
-        // currated
+        // save
+        
+        Storage::put('decks.json', str_finish(json_encode($meta, JSON_PRETTY_PRINT), PHP_EOL));
     }
     
     protected function determineLocalesPosition($locales, $meta_locales)
@@ -135,43 +144,53 @@ class GenerateDecksMetaFile extends Command
         $count = $set->count();
         
         for ($i = 0; $i < $count; $i++) {
+            $suggested_index = count($meta);
+            
             foreach (Counter::loop($meta) as $id => $deck) {
                 $this->info(' [<comment>' . Counter::i() . '</comment>] </info>' . $id . '<info> => </info>' . $deck['name'] . '<info>');
+                
+                $meta[$id]['index'] = Counter::i();
+                
+                if ($deck['name'] == $set[$i]['name']) {
+                    $suggested_index = Counter::i();
+                }
             }
             
-            $deck['id'] = $id;
+            $deck = $set[$i];
             
             if (! count($meta)) {
-                $deck = $set[$i];
-                $deck['index'] = $i;
-                $set[$i] = $deck;
-                
-                $meta[$set[$i]['id']] = $set[$i];
+                $index = $i;
             } else {
-                $deck = $set[$i];
-                $index = $this->ask('Deck index for <comment>' . $deck['name'] . '</comment>', count($meta));
-                
-                $set = $set->map(function ($deck) use ($index) {
-                    if (isset($deck['index']) && $deck['index'] >= $index) {
-                        $deck['index']++;
-                    }
-                    
-                    return $deck;
-                });
-                
-                $deck['index'] = $index;
-                $set[$i] = $deck;
-                
-                $meta[$set[$i]['id']] = $set[$i];
+                $index = $this->ask('Deck index for <comment>' . $deck['id'] . ' => ' . $deck['name'] . '</comment>', $suggested_index);
             }
             
-            $set = $set->sortBy('index');
-            $meta = $set->filter(function ($deck) {
-                return isset($deck['index']);
-            })->keyBy('id')->all();
+            $set = $set->map(function ($deck) use ($index) {
+                if (isset($deck['index']) && $deck['index'] >= $index) {
+                    $deck['index']++;
+                }
+                
+                return $deck;
+            });
+            
+            $deck['index'] = $index;
+            $set[$i] = $deck;
+            
+            $id = $set[$i]['id'];
+            
+            $meta = collect($meta)->map(function ($deck, $meta_id) use ($index, $id) {
+                if ($meta_id != $id && isset($deck['index']) && $deck['index'] >= $index) {
+                    $deck['index']++;
+                }
+                
+                return $deck;
+            })->all();
+            
+            $meta[$id] = $deck;
+            
+            $meta = collect($meta)->sortBy('index')->all();
         }
         
-        return $set;
+        return $set->sortBy('index');
     }
     
     protected function getMeta($deck, $meta)
